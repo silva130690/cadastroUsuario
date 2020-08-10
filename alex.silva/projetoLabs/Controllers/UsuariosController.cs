454816e07fc6c6d4a12dc9dd2b3a31ce45c46501
silva130690 <alex13_suporte@hotmail.com>
2020-08-10 01:07:44 -0300

    adicioanando o projeto para apresentacao

---
+++ alex.silva/projetoLabs/Controllers/UsuariosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication1.Models.Contexto;
+using WebApplication1.Models.Entidades;
+
+namespace WebApplication1.Controllers
+{
+    public class UsuariosController : Controller
+    {
+
+        private readonly Contexto _contexto;
+
+        public UsuariosController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public IActionResult Index()
+        {
+            var lista = _contexto.Usuario.ToList();
+            CarregaTipoUsuario();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var usuario = new Usuario();
+            CarregaTipoUsuario();
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Usuario.Add(usuario);
+                _contexto.SaveChanges();
+
+                return RedirectToAction("Página Inicial");
+            }
+
+            CarregaTipoUsuario();
+            return View(usuario);
+        }
+        
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            var usuario = _contexto.Usuario.Find(Id);
+
+            CarregaTipoUsuario();
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Usuario.Update(usuario);
+                _contexto.SaveChanges();
+
+                return RedirectToAction("Página Inicial");
+            }
+            else
+            {
+                CarregaTipoUsuario();
+                return View(usuario);
+            }
+        }
+        
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            var usuario = _contexto.Usuario.Find(Id);
+            CarregaTipoUsuario();
+            return View(usuario);
+        }
+        
+        [HttpPost]
+        public IActionResult Delete(Usuario _usuario)
+        {
+            var usuario = _contexto.Usuario.Find(_usuario.Id);
+            if (usuario != null)
+            {
+                _contexto.Usuario.Remove(usuario);
+                _contexto.SaveChanges();
+
+                return RedirectToAction("Página Inicial");
+            }
+            return View(usuario);
+        }
+        
+        [HttpGet]
+        public IActionResult Details(int Id)
+        {
+            var usuario = _contexto.Usuario.Find(Id);
+            CarregaTipoUsuario();
+            return View(usuario);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void CarregaTipoUsuario()
+        {
+            //Lista de Tipos de perfis de usuario
+            var ItensTipoUsuario = new List<SelectListItem>
+            {
+                new SelectListItem{ Value ="1", Text ="Administrador"},
+                 new SelectListItem{ Value ="2", Text ="Técnico"},
+                  new SelectListItem{ Value ="3", Text ="Usuário Normal"}
+            };
+
+            ViewBag.TiposUsuario = ItensTipoUsuario;
+        }
+
+
+
+
+    }
+}
---
+++ alex.silva/projetoLabs/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.Models.Contexto;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+// Este método é chamado pelo tempo de execução utilizado  para adicionar serviços ao contêiner.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<Contexto>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc();
+        }
+
+// Este método é chamado pelo tempo de execução para configurar o pipeline de solicitação HTTP.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Usuarios}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
